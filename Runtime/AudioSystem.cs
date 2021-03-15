bbb810a84dbb5adb2239e9fe9e514946157f3f9d
Eric Marchetti <ericmsbr@hotmail.com>
2021-03-15 16:19:38 -0300

    Added assets extrated from external project

---
+++ Runtime/AudioSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Audio System", menuName = "Inside/Audio/Audio System")]
+public class AudioSystem : ScriptableObject
+{
+    private AudioSystemBehaviour _controllerBehaviour;
+    private AudioSystemBehaviour ControllerBehaviour
+    {
+        get
+        {
+            if (_controllerBehaviour == null)
+            {
+                _controllerBehaviour = AudioSystemBehaviour.CreateNew(this);
+            }
+            return _controllerBehaviour;
+        }
+        set
+        {
+            _controllerBehaviour = value;
+        }
+    }
+
+    public void Initialize()
+    {
+        _ = ControllerBehaviour;
+    }
+
+
+
+    public void PlayAudio(AudioPlayOrder order)
+    {
+        ControllerBehaviour.PlayAudio(order);
+    }
+
+    public void StopAudio(AudioPlayOrder order)
+    {
+        ControllerBehaviour.StopAudio(order);
+    }
+
+
+    [Header("Audio Source Pool")]
+    [SerializeField] private int _initialPoolSize = default;
+    [SerializeField] private AudioSource _audioSourcePrefab = default;
+    public int InitialPoolSize => _initialPoolSize;
+    public AudioSource AudioSourcePrefab => _audioSourcePrefab;
+
+}
---
+++ Runtime/AudioSystemBehaviour.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Inside.Pool;
+
+public class AudioSystemBehaviour : MonoBehaviour
+{
+    public AudioSystem System { get; private set; }
+    private List<AudioPlayOrder> _ongoingOrders;
+
+    private ComponentPool<AudioSource> _pool;
+
+    public static AudioSystemBehaviour CreateNew(AudioSystem system)
+    {
+        AudioSystemBehaviour instance = new GameObject()
+            .AddComponent<AudioSystemBehaviour>();
+        instance.System = system;
+        DontDestroyOnLoad(instance.gameObject);
+        instance.gameObject.name = $"{typeof(AudioSystemBehaviour)}";
+        instance.Initialize();
+        return instance;
+    }
+
+    private void Initialize()
+    {
+        _ongoingOrders = new List<AudioPlayOrder>();
+
+        _pool = new ComponentPool<AudioSource>()
+        {
+            Prefab = System.AudioSourcePrefab
+        };
+        _pool.SetParent(transform);
+        _pool.Prewarm(System.InitialPoolSize);
+    }
+
+    public void PlayAudio(AudioPlayOrder order)
+    {
+        order.Source = _pool.Request();
+
+        order.Source.clip = order.Clip;
+        order.Source.loop = order.Loop;
+      
+
+        StartCoroutine(SourcePlayingRoutine(order));
+    }
+
+    public void StopAudio(AudioPlayOrder order)
+    {
+        if (order.State != AudioPlayOrder.PlayState.Playing
+            && order.State != AudioPlayOrder.PlayState.Paused)
+            return;
+
+        if (!_ongoingOrders.Contains(order))
+            return;
+        order.Source.Stop();
+        _pool.Return(order.Source);
+        _ongoingOrders.Remove(order);
+        order.UpdateState(AudioPlayOrder.PlayState.Stopped);
+        order.Source = null;
+    }
+
+    private IEnumerator SourcePlayingRoutine(AudioPlayOrder order)
+    {
+        order.Source.Play();
+        order.UpdateState(AudioPlayOrder.PlayState.Playing);
+        _ongoingOrders.Add(order);
+
+        while (order.Source != null &&
+            order.Source.isPlaying)
+        {
+            yield return new WaitForSeconds(order.Clip.length);
+        }
+
+        if(order.Source == null)
+        {
+            yield break;
+        }
+        _pool.Return(order.Source);
+        _ongoingOrders.Remove(order);
+        order.UpdateState(AudioPlayOrder.PlayState.Finished);
+        order.Source = null;
+    }
+}
